7b111df670ff0bd883aac193291dc4008695bdaf
root <root@yedu-dev.8.8.8.8>
2024-02-12 16:37:04 +0000

    some fixes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,38 +1,29 @@
 import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
-import env from "../../../env";
 
 export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }),
-    FacebookProvider({
-      clientId: env.FACEBOOK_CLIENT_ID,
-      clientSecret: env.FACEBOOK_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    async signIn(user, account, profile) {
-      const { email, name } = user;
-
-      console.log(email, name, account.access_token);
-
-      /*await fetch("https://your-server.com/api/user", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          name,
-          accessToken: account.access_token,
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
-      });*/
-
-      return { email, name, token: account.access_token };
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        }),
+    ],
+    callbacks: {
+        async signIn(info) {
+            const {user, account, profile} = info;
+            return {
+                email:user.email,
+                name:user.name,
+                token: account.access_token,
+                idToken: account.id_token:
+                provider: account.provider
+            };
+        },
     },
-  },
+    secret: process.env.SECRET
 });
