a7706ffce8445e64cf6b12bcf6d9bd71d1b14919
Alex Verbovskiy <cofeek5@gmail.com>
2024-02-19 12:57:32 +0200

    styles updates

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -7,8 +7,8 @@ const Layout = ({ children }) => {
         {/* Required meta tags */}
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
         <title>Indice - Directory & Listing React Next.js Template</title>
+
       </Head>
 
       {children}
--- pages/_app.js
+++ pages/_app.js
@@ -7,57 +7,69 @@ import Loader from "../components/Shared/Loader";
 import MainErrorAlert from "../components/_App/MainErrorAlert";
 import MainSuccessAlert from "../components/_App/MainSuccessAlert";
 import UnverifiedAlert from "../components/_App/UnverifiedAlert";
-import "../styles/index.css";
+import "../styles/admin/index.css";
 
 const useImportGlobalStyle = ({ type, onStart, onEnd }) => {
-  const stylesRef = useRef({ base: [], admin: [] });
-  const loadedRef = useRef({ base: false, admin: false });
+  const adminLinks = useRef([]);
   const isFirstCall = useRef(true);
 
-  const importStyle = async (importFunc, key) => {
-    stylesRef.current[key].forEach((elem) =>
-      document.querySelector("head").append(elem)
-    );
+  const loadStyles = () => [
+    `<link rel="stylesheet" href="/css/base/bootstrap.min.css"></link>`,
+    `<link rel="stylesheet" href="/css/base/animate.min.css"></link>`,
+    `<link rel="stylesheet" href="/css/base/style.css"></link>`,
+  ];
 
-    if (!loadedRef.current[key]) {
-      loadedRef.current[key] = true;
-      await importFunc();
+  const baseStyles = () => [
+    `<link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+  />`,
+    `<link rel="stylesheet" href="/css/base/bootstrap.min.css"></link>`,
+    `<link rel="stylesheet" href="/css/base/animate.min.css"></link>`,
+    `<link rel="stylesheet" href="/css/base/boxicons.min.css"></link>`,
+    `<link rel="stylesheet" href="/css/base/flaticon.css"></link>`,
+    `<link rel="stylesheet" href="/css/base/swiper.css"></link>`,
+    `<link rel="stylesheet" href="/css/base/swiper-bundle.css"></link>`,
+    `<link rel="stylesheet" href="/css/base/style.css"></link>`,
+    `<link rel="stylesheet" href="/css/base/responsive.css"></link>`,
+    `<link rel="stylesheet" href="/css/base/rtl.css"></link>`,
+  ];
 
-      document
-        .querySelectorAll("head style, head link")
-        .forEach((elem) => stylesRef.current[key].push(elem.cloneNode(true)));
-    }
-  };
+  const adminStyles = () => adminLinks.current;
 
   const onChangeType = async () => {
     onStart();
 
     if (isFirstCall.current) {
-      isFirstCall.current = false;
-
       document
         .querySelectorAll("head style, head link")
-        .forEach((elem, index) => {
-          if (
-            !elem.innerText.includes("MIT License | https://tailwindcss.com") &&
-            !elem.hasAttribute("data-n-p")
-          ) {
-            stylesRef.current["base"].push(elem.cloneNode(true));
-          }
-        });
+        .forEach((elem, index) => adminLinks.current.push(elem.outerHTML));
     }
 
-    document.querySelectorAll("head style, head link").forEach((elem) => {
-      elem.remove();
-    });
+    if (!isFirstCall.current || type != "admin") {
+      document
+        .querySelectorAll("head style, head link")
+        .forEach((elem) => elem.remove());
+    }
+
+    let linksToAdd = [];
 
     if (type == "admin") {
-      await importStyle(() => import(`../styles/admin/index.css`), "admin");
-    } else {
-      await importStyle(() => import(`../styles/index.css`), "base");
+      if (!isFirstCall.current) {
+        linksToAdd = adminStyles();
+      }
     }
 
-    onEnd();
+    if (type != "admin") {
+      linksToAdd = baseStyles();
+    }
+
+    linksToAdd.forEach((link) =>
+      document.querySelector("head").insertAdjacentHTML("beforeend", link)
+    );
+
+    isFirstCall.current = false;
+    setTimeout(onEnd, 10);
   };
 
   useEffect(() => {
@@ -66,7 +78,7 @@ const useImportGlobalStyle = ({ type, onStart, onEnd }) => {
 };
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const pageType = pageProps.pageType;
   const user = pageProps.user;
@@ -80,9 +92,13 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <SessionProvider>
-      <IndiceProvider authToken={authToken} userInfo={user} dopProps={{ setLoading }}>
+      <IndiceProvider
+        authToken={authToken}
+        userInfo={user}
+        dopProps={{ setLoading }}
+      >
         <Layout>
-          <Component {...pageProps} />
+          {!loading && <Component {...pageProps} />}
 
           <Loader loading={loading} />
 
