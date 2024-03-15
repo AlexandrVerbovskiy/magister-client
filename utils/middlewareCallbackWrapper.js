e2814b17bf8ab2d00369943f9b3ac30cb7afc9ff
Alex Verbovskiy <cofeek5@gmail.com>
2024-03-15 09:48:40 +0200

    some update

--- utils/middlewareCallbackWrapper.js
+++ utils/middlewareCallbackWrapper.js
@@ -1,21 +1,21 @@
 const middlewareCallbackWrapper = async ({ callback, res, context }) => {
   if (callback) {
-    try {
-      const callbackRes = await callback({ baseSideProps: res, context });
+    //try {
+    const callbackRes = await callback({ baseSideProps: res, context });
 
-      if (callback.notFound) {
-        return {
-          notFound: true,
-        };
-      }
-
-      res = { ...res, ...callbackRes };
-    } catch (e) {
-      console.error("Middleware error: " + e.message);
+    if (callback.notFound) {
       return {
         notFound: true,
       };
     }
+
+    res = { ...res, ...callbackRes };
+    /*} catch (e) {
+      console.error("Middleware error: " + e.message);
+      return {
+        notFound: true,
+      };
+    }*/
   }
 
   return { props: res };
