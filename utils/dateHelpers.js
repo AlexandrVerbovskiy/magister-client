00b39924f717d121b68b76516707890b1fa867c5
Alex Verbovskiy <cofeek5@gmail.com>
2024-03-27 18:10:43 +0200

    some fixes

--- utils/dateHelpers.js
+++ utils/dateHelpers.js
@@ -9,9 +9,6 @@ export const timeConverter = (time) => {
   const dateObject = new Date(time);
 
   const formattedDate = dateObject.toLocaleDateString("en-US");
-  const formattedTime = dateObject.toLocaleTimeString("en-US", {
-    hour12: false,
-  });
 
   const formattedDateParts = formattedDate.split("/");
 
@@ -20,7 +17,7 @@ export const timeConverter = (time) => {
     `${formattedDateParts[1].length < 2 ? "0" : ""}${formattedDateParts[1]}/` +
     `${formattedDateParts[2]}`;
 
-  return `${fullFormattedDate} ${formattedTime}`;
+  return `${fullFormattedDate}`;
 };
 
 export const timeNormalConverter = (time) => {
@@ -29,11 +26,8 @@ export const timeNormalConverter = (time) => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
 
-  return `${month}/${day}/${year} ${hours}:${minutes}:${seconds}`;
+  return `${month}/${day}/${year}`;
 };
 
 export const timeName = () => {
