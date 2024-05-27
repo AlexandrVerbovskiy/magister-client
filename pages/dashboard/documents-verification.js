8ce45001681b8e83f02d8ff4ccfaa8dc52b802e9
Alex Verbovskiy <cofeek5@gmail.com>
2024-05-27 13:43:46 +0300

    some updates

--- pages/dashboard/documents-verification.js
+++ pages/dashboard/documents-verification.js
@@ -20,6 +20,8 @@ const DocumentsVerification = ({
   lastAnswerDescription,
   canChange,
 }) => {
+  console.log(lastAnswerDescription);
+
   const [formError, setFormError] = useState(null);
   const { success, setLoading, sessionUser, authToken, setVerified, error } =
     useContext(IndiceContext);
