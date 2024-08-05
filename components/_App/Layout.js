8281230d46776ce98018c994eb2f2b828123fe6d
Alex Verbovskiy <cofeek5@gmail.com>
2024-08-05 13:24:16 +0300

    start navbar updates

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -7,12 +7,14 @@ const Layout = ({ children }) => {
         {/* Required meta tags */}
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Indice - Directory & Listing React Next.js Template</title>
+        <title>RentAbout</title>
         <link
           rel="shortcut icon"
           type="image/x-icon"
           href="/images/rent-about-logo.ico"
         />
+        <meta name="description" content="RentAbout is a great site"/>
+        <meta name="keywords" content="RentAbout"/>
       </Head>
 
       {children}
