04f1f97185cffef5cc86b79423b86aab2d487863
Alex Verbovskiy <cofeek5@gmail.com>
2024-08-22 16:11:11 +0300

    some updates

--- components/admin/Chat/OrderUpdateStatusMessageContent.js
+++ components/admin/Chat/OrderUpdateStatusMessageContent.js
@@ -2,11 +2,10 @@ const OrderUpdateStatusMessageContent = ({
   title,
   messageClassName,
   Icon = null,
+  description = null,
 }) => {
   return (
-    <div
-      className={`flex flex-col items-center ${messageClassName}`}
-    >
+    <div className={`flex flex-col items-center ${messageClassName}`}>
       <div className="flex items-center">
         <b>{title}</b>
         {Icon && (
@@ -17,6 +16,7 @@ const OrderUpdateStatusMessageContent = ({
           />
         )}
       </div>
+      {description && <div className="w-full mb-1">{description}</div>}
     </div>
   );
 };
