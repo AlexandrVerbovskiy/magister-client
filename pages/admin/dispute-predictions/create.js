4445114f002b1b30789938dc621b2e40a7b3e0f8
Alex Verbovskiy <cofeek5@gmail.com>
2025-07-01 22:33:07 +0300

    prediction started

--- pages/admin/dispute-predictions/create.js
+++ pages/admin/dispute-predictions/create.js
@@ -1,12 +1,116 @@
+import { useAdminPage } from "../../../hooks";
+import BreadCrumbs from "../../../partials/admin/base/BreadCrumbs";
+import Header from "../../../partials/admin/Header";
+import Sidebar from "../../../partials/admin/Sidebar";
 import { getTableRelations } from "../../../services";
 import { adminSideProps } from "../../../middlewares";
 import { useState } from "react";
-import EditForm from "../../../components/admin/DisputePrediction/EditForm";
+import { cloneObject } from "../../../utils";
+import ModelParamModal from "../../../components/admin/DisputePrediction/ModelParamModal";
 
-const CreateDisputePrediction = ({ structure }) => {
-  const [base, setBase] = useState(null);
+const CreateDisputePrediction = (pageProps) => {
+  const { sidebarOpen, setSidebarOpen } = useAdminPage();
+  console.log(pageProps);
+  const [modelParams, setModelParams] = useState([]);
+  const [disabled, setDisabled] = useState(false);
+  const [activeModelParam, setActiveModelParam] = useState(null);
 
-  return <EditForm structure={structure} base={base} setBase={setBase} />;
+  const handleSubmit = async () => {
+    console.log(modelParams);
+  };
+
+  const updateModelParam = (index) => {
+    modelParams[index];
+    setActiveModelParam({
+      index,
+      fieldName: modelParams[index].fieldName,
+      content: cloneObject(modelParams[index].content),
+    });
+  };
+
+  const createModelParam = () => {
+    setActiveModelParam({ fieldName: "", content: [] });
+  };
+
+  const saveModelParam = (param, index = null) => {
+    if (index === null) {
+      setModelParams((prev) => [...prev, param]);
+      return;
+    }
+
+    setModelParams((prev) => {
+      prev.map((prevModelParam, prevModelParamIndex) => {
+        return prevModelParamIndex === index ? param : prevModelParam;
+      });
+    });
+  };
+
+  return (
+    <div className="flex h-[100dvh] overflow-hidden">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden dark:bg-slate-900">
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+        <main className="grow">
+          <div className="relative">
+            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+              <div className="md:flex md:justify-between md:items-center mb-8">
+                <BreadCrumbs
+                  links={[
+                    {
+                      title: "Dispute Predictions",
+                      href: "/admin/dispute-predictions",
+                    },
+                    { title: "Create" },
+                  ]}
+                />
+              </div>
+
+              <div className="mt-8"></div>
+
+              <div className="bg-white dark:bg-slate-800 shadow-lg rounded-sm mb-8">
+                <div className="flex flex-col md:flex-row md:-mr-px">
+                  <div className="grow w-full">
+                    <footer>
+                      <div className="flex flex-col px-6 py-5 border-t border-slate-200 dark:border-slate-700">
+                        <div className="flex self-end">
+                          <button
+                            onClick={createModelParam}
+                            className="btn bg-blue-500 hover:bg-blue-600 text-white"
+                          >
+                            Add New Param
+                          </button>
+
+                          <button
+                            disabled={disabled}
+                            type="button"
+                            onClick={handleSubmit}
+                            className="btn bg-teal-500 hover:bg-teal-600 text-white ml-3"
+                          >
+                            Create Model
+                          </button>
+                        </div>
+                      </div>
+                    </footer>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+
+      <ModelParamModal
+        onSaveClick={saveModelParam}
+        modalOpen={!!activeModelParam}
+        closeModal={() => setActiveModelParam(null)}
+        index={activeModelParam?.index ?? null}
+        content={activeModelParam?.content ?? []}
+        fieldName={activeModelParam?.fieldName ?? ""}
+      />
+    </div>
+  );
 };
 
 const boostServerSideProps = async ({ baseSideProps }) => {
--- pages/admin/dispute-predictions/index.js
+++ pages/admin/dispute-predictions/index.js
@@ -1,92 +1,12 @@
-import React, { useContext, useState } from "react";
-import Sidebar from "../../../partials/admin/Sidebar";
-import Header from "../../../partials/admin/Header";
+import { useAdminPage } from "../../../hooks";
 import BreadCrumbs from "../../../partials/admin/base/BreadCrumbs";
-import PaginationNumeric from "../../../components/admin/PaginationNumeric";
-import DisputePredictionModelsTable from "../../../components/admin/DisputePrediction/Table";
+import Header from "../../../partials/admin/Header";
+import Sidebar from "../../../partials/admin/Sidebar";
 import { adminSideProps } from "../../../middlewares";
-import { useAdminPage, usePagination } from "../../../hooks";
-import { IndiceContext } from "../../../contexts";
-import {
-  getAdminDisputePredictionModelListPageOptions,
-  getDisputePredictionModelList,
-  stopDisputePredictionModel,
-  unstopDisputePredictionModel,
-  activateDisputePredictionModel,
-  startTrainingDisputePredictionModel,
-} from "../../../services";
-import { baseListPageParams } from "../../../utils";
-import YesNoModal from "../../../components/admin/YesNoModal";
-import Link from "next/link";
-import ModalBlank from "../../../components/admin/ModalBlank";
-import Switch from "../../../partials/admin/base/Switch";
 
-const DisputePredictionModels = (pageProps) => {
+const DisputePredictionList = (pageProps) => {
   const { sidebarOpen, setSidebarOpen } = useAdminPage();
-  const { error, authToken } = useContext(IndiceContext);
-
-  const [modelIdToStop, setModelIdToStop] = useState(null);
-  const [modelIdToUnstop, setModelIdToUnstop] = useState(null);
-  const [modelIdToActivate, setModelIdToActivate] = useState(null);
-  const [modelIdToStartTraining, setModelIdToStartTraining] = useState(null);
-
-  const [onActivateRebuild, setOnActivateRebuild] = useState(false);
-
-  const {
-    page,
-    countItems,
-    countPages,
-    moveToPage,
-    order,
-    orderType,
-    currentTo,
-    currentFrom,
-    handleChangeOrder,
-    canMoveNextPage,
-    canMovePrevPage,
-    items: models,
-    rebuild,
-    loading: paginationLoading,
-  } = usePagination({
-    getItemsFunc: (data) => getDisputePredictionModelList(data, authToken),
-    onError: (e) => error.set(e.message),
-    defaultData: pageProps,
-  });
-
-  const onAcceptStopModel = async () => {
-    await stopDisputePredictionModel(modelIdToStop, authToken);
-    setModelIdToStop(null);
-    rebuild();
-  };
-
-  const onAcceptUnstopModel = async () => {
-    await unstopDisputePredictionModel(modelIdToUnstop, authToken);
-    setModelIdToUnstop(null);
-    rebuild();
-  };
-
-  const onAcceptActivateModel = async () => {
-    await activateDisputePredictionModel(
-      modelIdToActivate,
-      onActivateRebuild,
-      authToken
-    );
-    setModelIdToActivate(null);
-    setOnActivateRebuild(false);
-    rebuild();
-  };
-
-  const onStartTrainingModel = async () => {
-    const model = models.find((model) => model.id === modelIdToStartTraining);
-
-    await startTrainingDisputePredictionModel(
-      modelIdToStartTraining,
-      model.selectedFields,
-      authToken
-    );
-    setModelIdToStartTraining(null);
-    rebuild();
-  };
+  console.log(pageProps);
 
   return (
     <div className="flex h-[100dvh] overflow-hidden">
@@ -99,156 +19,22 @@ const DisputePredictionModels = (pageProps) => {
           <div className="relative">
             <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
               <div className="md:flex md:justify-between md:items-center mb-8">
-                <BreadCrumbs links={[{ title: "Dispute Prediction Models" }]} />
-
-                <div className="flex md:auto-cols-max justify-start md:justify-end gap-2 mt-2 md:mt-0 flex-col md:flex-row">
-                  <div className="flex gap-2">
-                    <Link
-                      href="/admin/dispute-predictions/create/"
-                      className="btn bg-teal-500 hover:bg-teal-600 text-white"
-                    >
-                      <svg
-                        className="w-4 h-4 fill-current opacity-50 shrink-0"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-                      </svg>
-                      <span className="ml-2">Add Model</span>
-                    </Link>
-                  </div>
-                </div>
+                <BreadCrumbs links={[{ title: "Dispute Predictions" }]} />
               </div>
 
-              <DisputePredictionModelsTable
-                models={models}
-                orderField={order}
-                orderType={orderType}
-                onClickTh={handleChangeOrder}
-                totalCount={countItems}
-                loading={paginationLoading}
-                setModelIdToStop={setModelIdToStop}
-                setModelIdToUnstop={setModelIdToUnstop}
-                setModelIdToActivate={setModelIdToActivate}
-                setModelIdToStartTraining={setModelIdToStartTraining}
-              />
-
-              <div className="mt-8">
-                <PaginationNumeric
-                  page={page}
-                  countPages={countPages}
-                  move={moveToPage}
-                  canNext={canMoveNextPage}
-                  canPrev={canMovePrevPage}
-                  to={currentTo}
-                  from={currentFrom}
-                  totalCount={countItems}
-                />
-              </div>
+              <div className="mt-8"></div>
             </div>
           </div>
         </main>
       </div>
-
-      <YesNoModal
-        title="Confirm action"
-        modalOpen={!!modelIdToStop}
-        handleCloseModal={() => modelIdToStop(null)}
-        body={`Are you sure you want to stop generating model #${modelIdToStop}?`}
-        onAccept={onAcceptStopModel}
-      />
-
-      <YesNoModal
-        title="Confirm action"
-        modalOpen={!!modelIdToUnstop}
-        handleCloseModal={() => setModelIdToUnstop(null)}
-        body={`Are you sure you want to continue generating model #${modelIdToUnstop}?`}
-        onAccept={onAcceptUnstopModel}
-        type="success"
-      />
-
-      <ModalBlank
-        id="modelIdToActivate"
-        modalOpen={!!modelIdToActivate}
-        setModalOpen={() => setModelIdToActivate(null)}
-      >
-        <div className="p-5 flex space-x-4">
-          <div className="w-10 h-10 rounded-full flex items-center justify-center shrink-0 bg-green-100 dark:bg-green-500/30">
-            <svg
-              className="w-4 h-4 shrink-0 fill-current text-green-500"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 12c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1zm1-3H7V4h2v5z" />
-            </svg>
-          </div>
-
-          <div className="w-full">
-            <div className="mb-2">
-              <div className="text-lg font-semibold text-slate-800 dark:text-slate-100">
-                Confirm action
-              </div>
-            </div>
-            <div className="text-sm mb-2">
-              <div className="space-y-2">
-                <p>
-                  Are you sure you want to activate model #{modelIdToActivate}?
-                </p>
-
-                <Switch
-                  id="after_finish"
-                  checked={onActivateRebuild}
-                  changeChecked={() => setOnActivateRebuild(!onActivateRebuild)}
-                  onText="Rebuild"
-                  offText="No Rebuild"
-                />
-              </div>
-            </div>
-            <div className="flex flex-wrap justify-end space-x-2">
-              <button
-                className="btn border-slate-200 dark:border-slate-700 hover:border-slate-300 dark:hover:border-slate-600 text-slate-600 dark:text-slate-300"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setModelIdToActivate(null);
-                }}
-              >
-                No
-              </button>
-              <button
-                onClick={onAcceptActivateModel}
-                className="btn bg-green-500 hover:bg-green-600 text-white"
-              >
-                Yes
-              </button>
-            </div>
-          </div>
-        </div>
-      </ModalBlank>
-
-      <YesNoModal
-        title="Start training"
-        modalOpen={!!modelIdToStartTraining}
-        handleCloseModal={() => setModelIdToStartTraining(null)}
-        body={`Are you sure you want to start training model #${modelIdToUnstop}?`}
-        onAccept={onStartTrainingModel}
-        type="success"
-      />
     </div>
   );
-};
-
-const boostServerSideProps = async ({ context, baseSideProps }) => {
-  const options = await getAdminDisputePredictionModelListPageOptions(
-    baseListPageParams(context.query),
-    baseSideProps.authToken
-  );
-
-  return { ...options };
 };
 
 export const getServerSideProps = (context) =>
   adminSideProps({
     context,
-    callback: boostServerSideProps,
-    baseProps: { pageTitle: "Dispute Prediction Models" },
+    baseProps: { pageTitle: "Dispute Predictions" },
   });
 
-export default DisputePredictionModels;
+export default DisputePredictionList;
