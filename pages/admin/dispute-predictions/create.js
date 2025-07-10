dc2186b091bbda04ac14b91101801f0463d2db79
Alex Verbovskiy <cofeek5@gmail.com>
2025-07-10 22:02:02 +0300

    template condition compelted

--- pages/admin/dispute-predictions/create.js
+++ pages/admin/dispute-predictions/create.js
@@ -9,7 +9,7 @@ import { cloneObject } from "../../../utils";
 import ModelParamTemplateModal from "../../../components/admin/DisputePrediction/ModelParamTemplateModal";
 import ModelParamFieldModal from "../../../components/admin/DisputePrediction/ModelParamFieldModal";
 
-const CreateDisputePrediction = ({structure: tableStructure}) => {
+const CreateDisputePrediction = ({ structure: tableStructure }) => {
   const { sidebarOpen, setSidebarOpen } = useAdminPage();
   const [modelParams, setModelParams] = useState([]);
   const [disabled, setDisabled] = useState(false);
@@ -22,17 +22,44 @@ const CreateDisputePrediction = ({structure: tableStructure}) => {
   };
 
   const updateModelParam = (index) => {
-    setActiveModelTemplateParam({
-      index,
-      pseudonym: modelParams[index].pseudonym,
-      content: cloneObject(modelParams[index].content),
-    });
+    if (modelParams[index]["type"] === "template") {
+      setActiveModelTemplateParam({
+        index,
+        pseudonym: modelParams[index].pseudonym,
+        content: cloneObject(modelParams[index].content),
+        condition: {
+          mainTable: modelParams[index].mainTable ?? null,
+          mainField: modelParams[index].mainField ?? null,
+          operation: modelParams[index].operation ?? null,
+          subTable: modelParams[index].subTable ?? null,
+          subField: modelParams[index].subField ?? null,
+        },
+      });
+    }
+
+    if (modelParams[index]["type"] === "field") {
+      setActiveModelFieldParam({
+        index,
+        pseudonym: modelParams[index].pseudonym,
+        tableName: modelParams[index].content.tableName,
+        fieldName: modelParams[index].content.fieldName,
+        connectTableName: modelParams[index].content.connectTableName,
+        connectFieldName: modelParams[index].content.connectFieldName,
+      });
+    }
   };
 
   const createModelTemplateParam = () => {
     setActiveModelTemplateParam({
       pseudonym: "",
       content: [],
+      condition: {
+        mainTable: null,
+        mainField: null,
+        operation: null,
+        subTable: null,
+        subField: null,
+      },
     });
   };
 
@@ -41,37 +68,24 @@ const CreateDisputePrediction = ({structure: tableStructure}) => {
       pseudonym: "",
       tableName: "",
       fieldName: "",
+      connectTableName: "",
+      connectFieldName: "",
     });
   };
 
-  const saveModelTemplateParam = (param, index = null) => {
+  const saveModelParam = (param, index = null) => {
     if (index === null) {
       setModelParams((prev) => [...prev, param]);
       return;
     }
 
-    setModelParams((prev) => {
+    setModelParams((prev) =>
       prev.map((prevModelParam, prevModelParamIndex) => {
         return prevModelParamIndex === index
           ? { ...prevModelParam, ...param }
           : prevModelParam;
-      });
-    });
-  };
-
-  const saveModelFieldParam = (param, index = null) => {
-    if (index === null) {
-      setModelParams((prev) => [...prev, param]);
-      return;
-    }
-
-    setModelParams((prev) => {
-      prev.map((prevModelParam, prevModelParamIndex) => {
-        return prevModelParamIndex === index
-          ? { ...prevModelParam, ...param }
-          : prevModelParam;
-      });
-    });
+      })
+    );
   };
 
   return (
@@ -81,9 +95,9 @@ const CreateDisputePrediction = ({structure: tableStructure}) => {
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden dark:bg-slate-900">
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-        <main className="grow">
-          <div className="relative">
-            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+        <main className="grow overflow-hidden">
+          <div className="relative h-full">
+            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto h-full flex flex-col justify-space-between overflow-hidden">
               <div className="md:flex md:justify-between md:items-center mb-8">
                 <BreadCrumbs
                   links={[
@@ -96,9 +110,18 @@ const CreateDisputePrediction = ({structure: tableStructure}) => {
                 />
               </div>
 
-              <div className="mt-8"></div>
+              <div className="bg-white dark:bg-slate-800 shadow-lg rounded-sm flex flex-col justify-space-between h-full overflow-hidden">
+                <div className="overflow-auto max-h-full h-full gap-2 flex flex-col m-4 ">
+                  {modelParams.map((param, index) => (
+                    <div
+                      className="p-4 rounded-xs border border-slate-200 cursor-pointer hover:bg-gray-100 transition duration-150 ease-in-out"
+                      onClick={() => updateModelParam(index)}
+                    >
+                      {param.pseudonym}
+                    </div>
+                  ))}
+                </div>
 
-              <div className="bg-white dark:bg-slate-800 shadow-lg rounded-sm mb-8">
                 <div className="flex flex-col md:flex-row md:-mr-px">
                   <div className="grow w-full">
                     <footer>
@@ -138,23 +161,27 @@ const CreateDisputePrediction = ({structure: tableStructure}) => {
       </div>
 
       <ModelParamTemplateModal
-        onSaveClick={saveModelTemplateParam}
+        onSaveClick={saveModelParam}
         modalOpen={!!activeModelTemplateParam}
         closeModal={() => setActiveModelTemplateParam(null)}
         index={activeModelTemplateParam?.index ?? null}
         content={activeModelTemplateParam?.content ?? []}
+        condition={activeModelTemplateParam?.condition ?? {}}
         pseudonym={activeModelTemplateParam?.pseudonym ?? ""}
+        tableStructure={tableStructure}
       />
 
       <ModelParamFieldModal
-        onSaveClick={saveModelFieldParam}
+        onSaveClick={saveModelParam}
         modalOpen={!!activeModelFieldParam}
         closeModal={() => setActiveModelFieldParam(null)}
         tableStructure={tableStructure}
-        index={activeModelTemplateParam?.index ?? null}
-        pseudonym={activeModelTemplateParam?.pseudonym ?? ""}
-        fieldName={activeModelTemplateParam?.fieldName ?? null}
-        tableName={activeModelTemplateParam?.tableName ?? null}
+        index={activeModelFieldParam?.index ?? null}
+        pseudonym={activeModelFieldParam?.pseudonym ?? ""}
+        fieldName={activeModelFieldParam?.fieldName ?? null}
+        tableName={activeModelFieldParam?.tableName ?? null}
+        connectFieldName={activeModelFieldParam?.connectFieldName ?? null}
+        connectTableName={activeModelFieldParam?.connectTableName ?? null}
       />
     </div>
   );
