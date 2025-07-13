743123c845bc13d2426979ec5c1a1e145cf663f1
Alex Verbovskiy <cofeek5@gmail.com>
2025-07-13 18:44:36 +0300

    modal completed

--- components/admin/DisputePrediction/OverlayItem.js
+++ components/admin/DisputePrediction/OverlayItem.js
@@ -1,49 +1,21 @@
-import React from "react";
 import STATIC from "../../../static";
-import TableSelect from "./TableSelect";
-
-const FieldBlock = ({ label, value }) => (
-  <div className="w-full sm:w-[calc(100%-4px)]">
-    <label className="block text-sm font-medium mb-1">{label}</label>
-    <div className="w-full btn justify-between min-w-44 bg-white dark:bg-slate-800 border-slate-200 dark:border-slate-700 hover:border-slate-300 dark:hover:border-slate-600 text-slate-500 hover:text-slate-600 dark:text-slate-300 dark:hover:text-slate-200">
-      {value || `Select ${label.toLowerCase()}`}
-    </div>
-  </div>
-);
 
 const ItemTree = ({ item }) => {
   if (item.key === STATIC.DISPUTE_PREDICTION_BLOCK.CUSTOM.TABLE_SELECTS.key) {
     return (
       <div
-        className="cursor-pointer p-2 mb-2 border border-slate-300"
-        style={{
-          width: "100%",
-          minHeight: 80,
-        }}
+        className="cursor-pointer p-2 text-sm mb-2 border border-slate-300 w-full"
+        style={{ transform: "none", zIndex: "auto", width: "100%" }}
       >
-        <div className="w-full mb-4 flex gap-2">
-          <FieldBlock label="Table Name" value={item.content.tableName} />
-          <FieldBlock label="Field Name" value={item.content.fieldName} />
-        </div>
-        <div className="w-full mb-4 flex gap-2">
-          <FieldBlock
-            label="Connection Table Name"
-            value={item.content.connectTableName}
-          />
-          <FieldBlock
-            label="Connection Field Name"
-            value={item.content.connectFieldName}
-          />
-        </div>
+        {item.content.pseudonym || "-"}
       </div>
     );
   }
 
   return (
     <div
-      className="cursor-pointer p-2 mb-2 border border-slate-300"
+      className="cursor-pointer p-2 mb-2 border border-slate-300 w-full"
       style={{
-        width: "100%",
         minHeight: 40 + (item.subItems?.length ? item.subItems.length * 40 : 0),
       }}
     >
